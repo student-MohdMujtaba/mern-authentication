14e32a9adacccde1f078ec58e2ac8b0a61e5d8db
Mohd Mujtaba <158465099+student-MohdMujtaba@users.noreply.github.com>
2025-03-18 04:19:08 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['https://mern-authentication-amber.vercel.app'];
 
 app.use(express.json());
 app.use(cookieParser());
